3fddb61571485800b10e0faea6d5bb184c710960
Sudhanva Naryana <nsudhanva@gmail.com>
2016-08-06 11:34:52 +0530

    Fixed SyntaxError in ProjectsController#index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,6 @@ class ApplicationController < ActionController::Base
   before_filter do
   	resource = controller_name.singularize.to_sym
   	method = "#{resource}_params"
-  	params[resource] &&= send(method) if respond_to? (method, true)
+  	params[resource] &&= send(method) if respond_to?(method, true)
   end
 end
