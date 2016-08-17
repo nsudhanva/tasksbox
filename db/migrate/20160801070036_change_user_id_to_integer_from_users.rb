519ad693c4e615e45c71c7e109ec4e51fdde7ecc
Sudhanva Naryana <nsudhanva@gmail.com>
2016-08-17 12:13:33 +0530

    Change migration

--- db/migrate/20160801070036_change_user_id_to_integer_from_users.rb
+++ db/migrate/20160801070036_change_user_id_to_integer_from_users.rb
@@ -1,6 +1,6 @@
 class ChangeUserIdToIntegerFromUsers < ActiveRecord::Migration
   def change
-  	remove_column :categories, :integer, :user_id
+  	remove_column :categories, :user_id, :string
   	add_column :categories, :user_id, :integer
   end
 end
