24ec35b30e40c9c35cb5960a919f91b353b9a0a9
Sudhanva Naryana <nsudhanva@gmail.com>
2016-09-21 17:17:13 +0530

    Final Commit

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,21 @@
 class RegistrationsController < Devise::RegistrationsController
 
+	def index
+		
+	end
+
+	def create
+		
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def update
+		
+	end
+
 	private
 
 	def sign_up_params
